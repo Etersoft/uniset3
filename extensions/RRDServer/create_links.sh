28cf7b0a1fa973d1fc2015e4e37ec84d1a9284e0
Pavel Vaynerman <pv@etersoft.ru>
2021-08-11 00:09:53 +0300

    [uniset-3.0]: removed RRDServer (deprecated)

--- extensions/RRDServer/create_links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ln -s -f ../../Utilities/scripts/uniset3-start.sh
-ln -s -f ../../Utilities/scripts/uniset3-stop.sh stop.sh
-ln -s -f ../../Utilities/scripts/uniset3-functions.sh
-ln -s -f ../../conf/test.xml test.xml
-
--- extensions/RRDServer/start_fg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./uniset3-start.sh -f ./uniset3-rrdserver --confile test.xml \
-	--rrd-name RRDServer1 \
-	--rrd-log-add-levels any $*
