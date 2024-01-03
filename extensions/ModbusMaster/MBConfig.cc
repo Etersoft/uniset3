Etersoft/uniset2

(modbusmaster): supported "mbmask"

extensions/ModbusMaster/MBConfig.cc
@@ -23,6 +23,7 @@
 #include <ORepHelpers.h>
 #include "MBConfig.h"
 #include "modbus/MBLogSugar.h"
+#include "MBExchange.h" // only for function 'firstBit'
 // -------------------------------------------------------------------------
 namespace uniset
 {
@@ -382,7 +383,7 @@ namespace uniset
             return true;
         }
 
-        const string sbit(IOBase::initProp(it, "nbit", prop_prefix, false));
+        const string sbit = IOBase::initProp(it, "nbit", prop_prefix, false);
 
         if( !sbit.empty() )
         {
@@ -396,15 +397,13 @@ namespace uniset
             }
         }
 
-        if( p.nbit > 0 &&
-                ( p.stype == UniversalIO::AI ||
-                  p.stype == UniversalIO::AO ) )
+        if( p.nbit > 0 && ( p.stype == UniversalIO::AI || p.stype == UniversalIO::AO ) )
         {
-            mbwarn << "(initRSProperty): (ignore) uncorrect param`s nbit!=0(" << p.nbit << ")"
+            mbwarn << "(initRSProperty): (ignore) incorrect param`s nbit!=0(" << p.nbit << ")"
                    << " for iotype=" << p.stype << " for " << it.getProp("name") << endl;
         }
 
-        const string sbyte(IOBase::initProp(it, "nbyte", prop_prefix, false) );
+        const string sbyte = IOBase::initProp(it, "nbyte", prop_prefix, false);
 
         if( !sbyte.empty() )
         {
@@ -418,7 +417,7 @@ namespace uniset
             }
         }
 
-        const string vt( IOBase::initProp(it, "vtype", prop_prefix, false) );
+        const string vt = IOBase::initProp(it, "vtype", prop_prefix, false);
 
         if( vt.empty() )
         {
@@ -440,6 +439,43 @@ namespace uniset
 
             p.vType = v;
             p.rnum = VTypes::wsize(v);
+        }
+
+        const string smask = IOBase::initProp(it, "mbmask", prop_prefix, false);
+
+        if( !smask.empty() )
+        {
+            if( p.nbit != -1 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: nbit!=0 and mbmask!=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            if( p.nbyte > 0 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: nbyte!=0 and mbmask!=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            uint16_t mask = uni_atoi(smask);
+
+            if( mask == 0 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: mbmask=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            p.offset = MBExchange::firstBit(mask);
+            p.mask = mask;
         }
 
         return true;
@@ -775,7 +811,7 @@ namespace uniset
             }
         }
 
-        // Фомируем список инициализации
+        // Формируем список инициализации
         bool need_init = IOBase::initIntProp(it, "preinit", prop_prefix, false);
 
         if( need_init && ModbusRTU::isWriteFunction(ri->mbfunc) )

extensions/ModbusMaster/tests/run_test_mbtcpmaster.cc
@@ -63,7 +63,7 @@ int main( int argc, const char* argv[] )
         int tout = conf->getArgPInt("--timeout", 8000);
         PassiveTimer pt(tout);
 
-        while( !pt.checkTime() && !act->exist() )
+        while( !pt.checkTime() && !act->exist() && !mbm->exist() )
             msleep(100);
 
         if( !act->exist() )

extensions/ModbusMaster/tests/test_mbcommon.cc
@@ -1,0 +1,53 @@
+#include <catch.hpp>
+// -----------------------------------------------------------------------------
+#include "MBExchange.h"
+// -----------------------------------------------------------------------------
+using namespace std;
+using namespace uniset;
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: first bit", "[modbus][firstbit]")
+{
+    REQUIRE( MBExchange::firstBit(4) == 2 );
+    REQUIRE( MBExchange::firstBit(1) == 0 );
+    REQUIRE( MBExchange::firstBit(64) == 6 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: get bits", "[modbus][getbits]")
+{
+    REQUIRE( MBExchange::getBits(1, 1, 0) == 1 );
+    REQUIRE( MBExchange::getBits(12, 12, 2) == 3 );
+    REQUIRE( MBExchange::getBits(9, 12, 2) == 2 );
+    REQUIRE( MBExchange::getBits(9, 3, 0) == 1 );
+    REQUIRE( MBExchange::getBits(15, 12, 2) == 3 );
+    REQUIRE( MBExchange::getBits(15, 3, 0) == 3 );
+    REQUIRE( MBExchange::getBits(15, 0, 0) == 15 );
+    REQUIRE( MBExchange::getBits(15, 0, 3) == 15 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: set bits", "[modbus][setbits]")
+{
+    REQUIRE( MBExchange::setBits(1, 1, 1, 0) == 1 );
+    REQUIRE( MBExchange::setBits(0, 3, 12, 2) == 12 );
+    REQUIRE( MBExchange::setBits(3, 3, 12, 2) == 15 );
+    REQUIRE( MBExchange::setBits(16, 3, 12, 2) == 28 );
+    REQUIRE( MBExchange::setBits(28, 1, 3, 0) == 29 );
+    REQUIRE( MBExchange::setBits(28, 10, 0, 0) == 28 );
+    REQUIRE( MBExchange::setBits(28, 10, 0, 4) == 28 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: force set bits", "[modbus][forcesetbits]")
+{
+    REQUIRE( MBExchange::forceSetBits(1, 1, 1, 0) == 1 );
+    REQUIRE( MBExchange::forceSetBits(0, 3, 12, 2) == 12 );
+    REQUIRE( MBExchange::forceSetBits(28, 10, 0, 0) == 10 );
+    REQUIRE( MBExchange::forceSetBits(28, 10, 0, 4) == 10 );
+
+    REQUIRE( MBExchange::forceSetBits(0, 2, 3, 0) == 2 );
+    REQUIRE( MBExchange::forceSetBits(2, 2, 12, 2) == 10 );
+
+    REQUIRE( MBExchange::forceSetBits(2, 0, 3, 0) == 0 );
+    REQUIRE( MBExchange::forceSetBits(12, 0, 12, 2) == 0 );
+
+    REQUIRE( MBExchange::forceSetBits(3, 5, 3, 0) == 1 );
+}
+// -----------------------------------------------------------------------------

Etersoft/uniset3

extensions/ModbusMaster/MBConfig.cc
@@ -22,6 +22,7 @@
 #include <extensions/Extensions.h>
 #include "MBConfig.h"
 #include "modbus/MBLogSugar.h"
+#include "MBExchange.h" // only for function 'firstBit'
 // -------------------------------------------------------------------------
 namespace uniset3
 {
@@ -378,7 +379,7 @@ namespace uniset3
             return true;
         }
 
-        const string sbit(IOBase::initProp(it, "nbit", prop_prefix, false));
+        const string sbit = IOBase::initProp(it, "nbit", prop_prefix, false);
 
         if( !sbit.empty() )
         {
@@ -392,15 +393,13 @@ namespace uniset3
             }
         }
 
-        if( p.nbit > 0 &&
-                ( p.stype == uniset3::AI ||
-                  p.stype == uniset3::AO ) )
+        if( p.nbit > 0 && ( p.stype == uniset3::AI || p.stype == uniset3::AO ) )
         {
-            mbwarn << "(initRSProperty): (ignore) uncorrect param`s nbit!=0(" << p.nbit << ")"
+            mbwarn << "(initRSProperty): (ignore) incorrect param`s nbit!=0(" << p.nbit << ")"
                    << " for iotype=" << p.stype << " for " << it.getProp("name") << endl;
         }
 
-        const string sbyte(IOBase::initProp(it, "nbyte", prop_prefix, false) );
+        const string sbyte = IOBase::initProp(it, "nbyte", prop_prefix, false);
 
         if( !sbyte.empty() )
         {
@@ -414,7 +413,7 @@ namespace uniset3
             }
         }
 
-        const string vt( IOBase::initProp(it, "vtype", prop_prefix, false) );
+        const string vt = IOBase::initProp(it, "vtype", prop_prefix, false);
 
         if( vt.empty() )
         {
@@ -436,6 +435,43 @@ namespace uniset3
 
             p.vType = v;
             p.rnum = VTypes::wsize(v);
+        }
+
+        const string smask = IOBase::initProp(it, "mbmask", prop_prefix, false);
+
+        if( !smask.empty() )
+        {
+            if( p.nbit != -1 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: nbit!=0 and mbmask!=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            if( p.nbyte > 0 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: nbyte!=0 and mbmask!=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            uint16_t mask = uni_atoi(smask);
+
+            if( mask == 0 )
+            {
+                mbcrit << myname << "(initRSProperty): config error: mbmask=0 for "
+                       << it.getProp("name")
+                       << endl;
+
+                return false;
+            }
+
+            p.offset = MBExchange::firstBit(mask);
+            p.mask = mask;
         }
 
         return true;
@@ -732,7 +768,7 @@ namespace uniset3
             }
         }
 
-        // Фомируем список инициализации
+        // Формируем список инициализации
         bool need_init = IOBase::initIntProp(it, "preinit", prop_prefix, false);
 
         if( need_init && ModbusRTU::isWriteFunction(ri->mbfunc) )

extensions/ModbusMaster/tests/run_test_mbtcpmaster.cc
@@ -61,7 +61,7 @@ int main( int argc, const char* argv[] )
         int tout = conf->getArgPInt("--timeout", 8000);
         PassiveTimer pt(tout);
 
-        while( !pt.checkTime() && !act->isExists() )
+        while( !pt.checkTime() && !act->isExists() && !mbm->isExists() )
             msleep(100);
 
         if( !act->isExists() )

extensions/ModbusMaster/tests/test_mbcommon.cc
@@ -1,0 +1,53 @@
+#include <catch.hpp>
+// -----------------------------------------------------------------------------
+#include "MBExchange.h"
+// -----------------------------------------------------------------------------
+using namespace std;
+using namespace uniset3;
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: first bit", "[modbus][firstbit]")
+{
+    REQUIRE( MBExchange::firstBit(4) == 2 );
+    REQUIRE( MBExchange::firstBit(1) == 0 );
+    REQUIRE( MBExchange::firstBit(64) == 6 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: get bits", "[modbus][getbits]")
+{
+    REQUIRE( MBExchange::getBits(1, 1, 0) == 1 );
+    REQUIRE( MBExchange::getBits(12, 12, 2) == 3 );
+    REQUIRE( MBExchange::getBits(9, 12, 2) == 2 );
+    REQUIRE( MBExchange::getBits(9, 3, 0) == 1 );
+    REQUIRE( MBExchange::getBits(15, 12, 2) == 3 );
+    REQUIRE( MBExchange::getBits(15, 3, 0) == 3 );
+    REQUIRE( MBExchange::getBits(15, 0, 0) == 15 );
+    REQUIRE( MBExchange::getBits(15, 0, 3) == 15 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: set bits", "[modbus][setbits]")
+{
+    REQUIRE( MBExchange::setBits(1, 1, 1, 0) == 1 );
+    REQUIRE( MBExchange::setBits(0, 3, 12, 2) == 12 );
+    REQUIRE( MBExchange::setBits(3, 3, 12, 2) == 15 );
+    REQUIRE( MBExchange::setBits(16, 3, 12, 2) == 28 );
+    REQUIRE( MBExchange::setBits(28, 1, 3, 0) == 29 );
+    REQUIRE( MBExchange::setBits(28, 10, 0, 0) == 28 );
+    REQUIRE( MBExchange::setBits(28, 10, 0, 4) == 28 );
+}
+// -----------------------------------------------------------------------------
+TEST_CASE("MBTCPMaster: force set bits", "[modbus][forcesetbits]")
+{
+    REQUIRE( MBExchange::forceSetBits(1, 1, 1, 0) == 1 );
+    REQUIRE( MBExchange::forceSetBits(0, 3, 12, 2) == 12 );
+    REQUIRE( MBExchange::forceSetBits(28, 10, 0, 0) == 10 );
+    REQUIRE( MBExchange::forceSetBits(28, 10, 0, 4) == 10 );
+
+    REQUIRE( MBExchange::forceSetBits(0, 2, 3, 0) == 2 );
+    REQUIRE( MBExchange::forceSetBits(2, 2, 12, 2) == 10 );
+
+    REQUIRE( MBExchange::forceSetBits(2, 0, 3, 0) == 0 );
+    REQUIRE( MBExchange::forceSetBits(12, 0, 12, 2) == 0 );
+
+    REQUIRE( MBExchange::forceSetBits(3, 5, 3, 0) == 1 );
+}
+// -----------------------------------------------------------------------------