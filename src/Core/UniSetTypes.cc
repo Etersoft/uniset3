Etersoft/uniset2

(uwebsocket): "copy optimization"

src/Core/UniSetTypes.cc
@@ -228,6 +228,11 @@ std::list<uniset::ObjectId> uniset::IDList::getList() const noexcept
     return lst;
 }
 
+const std::list<uniset::ObjectId>& uniset::IDList::ref() const noexcept
+{
+    return lst;
+}
+
 uniset::ObjectId uniset::IDList::getFirst() const noexcept
 {
     if( lst.empty() )

Etersoft/uniset3

src/Core/UniSetTypes.cc
@@ -228,6 +228,11 @@ std::list<uniset3::ObjectId> uniset3::IDList::getList() const noexcept
     return lst;
 }
 
+const std::list<uniset3::ObjectId>& uniset3::IDList::ref() const noexcept
+{
+    return lst;
+}
+
 uniset3::ObjectId uniset3::IDList::getFirst() const noexcept
 {
     if( lst.empty() )
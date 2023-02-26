8783b22f365bd9c9b92c3bf4ae4e35aff354b004
Pavel Vaynerman <pv@etersoft.ru>
2023-02-26 22:02:40 +0300

    (uwebsocket): "copy optimization"

--- include/UniSetTypes.h
+++ include/UniSetTypes.h
@@ -141,6 +141,7 @@ namespace uniset3
             }
 
             std::list<ObjectId> getList() const noexcept;
+            const std::list<ObjectId>& ref() const noexcept;
 
             // за освобождение выделенной памяти
             // отвечает вызывающий!
