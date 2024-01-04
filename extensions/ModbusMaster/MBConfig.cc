Etersoft/uniset2

(MBConfig): add throw exception after configuration error

extensions/ModbusMaster/MBConfig.cc
@@ -100,8 +100,12 @@ namespace uniset
 
         for( ; it.getCurrent(); it.goNext() )
         {
-            if( uniset::check_filter(it, s_field, s_fvalue) )
-                initItem(it);
+            if( uniset::check_filter(it, s_field, s_fvalue) && !initItem(it) )
+            {
+                ostringstream err;
+                err << myname << "(readConfiguration): Error during sensor configuration name -> " << it.getProp("name");
+                throw SystemError(err.str());
+            }
         }
 
         //    readconf_ok = true;

Etersoft/uniset3

extensions/ModbusMaster/MBConfig.cc
@@ -99,8 +99,12 @@ namespace uniset3
 
         for( ; it.getCurrent(); it.goNext() )
         {
-            if( uniset3::check_filter(it, s_field, s_fvalue) )
-                initItem(it);
+            if( uniset3::check_filter(it, s_field, s_fvalue) && !initItem(it) )
+            {
+                ostringstream err;
+                err << myname << "(readConfiguration): Error during sensor configuration name -> " << it.getProp("name");
+                throw SystemError(err.str());
+            }
         }
 
         //    readconf_ok = true;