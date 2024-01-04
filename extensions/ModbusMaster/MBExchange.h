Etersoft/uniset2

(extensions): supported  filterField=".." filtersValue=".." parameters in config

extensions/ModbusMaster/MBExchange.h
@@ -148,7 +148,7 @@ namespace uniset
             uniset::ObjectId sidHeartBeat = { uniset::DefaultObjectId };
             long maxHeartBeat = { 10 };
             IOController::IOStateList::iterator itHeartBeat;
-            uniset::ObjectId test_id = { uniset::DefaultObjectId };
+            uniset::ObjectId sidTestSMReady = {uniset::DefaultObjectId };
 
             uniset::ObjectId sidExchangeMode = { uniset::DefaultObjectId }; /*!< идентификатор для датчика режима работы */
             IOController::IOStateList::iterator itExchangeMode;

extensions/ModbusSlave/MBSlave.h
@@ -542,7 +542,7 @@ namespace uniset
             uniset::ObjectId sidHeartBeat = { uniset::DefaultObjectId };
             long maxHeartBeat = { 10 };
             IOController::IOStateList::iterator itHeartBeat;
-            uniset::ObjectId test_id = { uniset::DefaultObjectId };
+            uniset::ObjectId sidTestSMReady = {uniset::DefaultObjectId };
 
             IOController::IOStateList::iterator itAskCount;
             uniset::ObjectId askcount_id = { uniset::DefaultObjectId };
@@ -556,7 +556,7 @@ namespace uniset
             long restartTCPServerCount = { 0 };
 
             std::atomic_bool activated = { false };
-            std::atomic_bool cancelled = { false };
+            std::atomic_bool canceled = {false };
             timeout_t activateTimeout = { 20000 }; // msec
             bool smPingOK = { false };
             timeout_t wait_msec = { 3000 };

Etersoft/uniset3

extensions/ModbusMaster/MBExchange.h
@@ -143,7 +143,7 @@ namespace uniset3
             uniset3::ObjectId sidHeartBeat = { uniset3::DefaultObjectId };
             long maxHeartBeat = { 10 };
             IOController::IOStateList::iterator itHeartBeat;
-            uniset3::ObjectId test_id = { uniset3::DefaultObjectId };
+            uniset3::ObjectId sidTestSMReady = {uniset3::DefaultObjectId };
 
             uniset3::ObjectId sidExchangeMode = { uniset3::DefaultObjectId }; /*!< идентификатор для датчика режима работы */
             IOController::IOStateList::iterator itExchangeMode;

extensions/ModbusSlave/MBSlave.h
@@ -538,7 +538,7 @@ namespace uniset3
             uniset3::ObjectId sidHeartBeat = { uniset3::DefaultObjectId };
             long maxHeartBeat = { 10 };
             IOController::IOStateList::iterator itHeartBeat;
-            uniset3::ObjectId test_id = { uniset3::DefaultObjectId };
+            uniset3::ObjectId sidTestSMReady = {uniset3::DefaultObjectId };
 
             IOController::IOStateList::iterator itAskCount;
             uniset3::ObjectId askcount_id = { uniset3::DefaultObjectId };
@@ -552,7 +552,7 @@ namespace uniset3
             long restartTCPServerCount = { 0 };
 
             std::atomic_bool activated = { false };
-            std::atomic_bool cancelled = { false };
+            std::atomic_bool canceled = {false };
             timeout_t activateTimeout = { 20000 }; // msec
             bool smPingOK = { false };
             timeout_t wait_msec = { 3000 };