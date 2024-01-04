Etersoft/uniset2

(codegen): update "skel" (template)

Utilities/codegen/skel.cc
@@ -19,19 +19,27 @@ Skel::Skel()
     throw Exception();
 }
 // -----------------------------------------------------------------------------
+void Skel::sysCommand( const uniset::SystemMessage* sm )
+{
+
+}
+// -----------------------------------------------------------------------------
+void Skel::askSensors( UniversalIO::UIOCommand cmd )
+{
+
+}
+// -----------------------------------------------------------------------------
 void Skel::step()
 {
+
 }
 // -----------------------------------------------------------------------------
 void Skel::sensorInfo( const SensorMessage* sm )
 {
+
 }
 // -----------------------------------------------------------------------------
 void Skel::timerInfo( const TimerMessage* tm )
-{
-}
-// -----------------------------------------------------------------------------
-void Skel::askSensors( UniversalIO::UIOCommand cmd )
 {
 
 }

Etersoft/uniset3

Utilities/codegen/skel.cc
@@ -19,19 +19,27 @@ Skel::Skel()
     throw Exception();
 }
 // -----------------------------------------------------------------------------
-void Skel::step()
+void Skel::sysCommand( const umessage::SystemMessage* sm )
 {
-}
-// -----------------------------------------------------------------------------
-void Skel::sensorInfo( const SensorMessage* sm )
-{
-}
-// -----------------------------------------------------------------------------
-void Skel::timerInfo( const TimerMessage* tm )
-{
+
 }
 // -----------------------------------------------------------------------------
 void Skel::askSensors( uniset3::UIOCommand cmd )
+{
+
+}
+// -----------------------------------------------------------------------------
+void Skel::step()
+{
+
+}
+// -----------------------------------------------------------------------------
+void Skel::sensorInfo( const umessage::SensorMessage* sm )
+{
+
+}
+// -----------------------------------------------------------------------------
+void Skel::timerInfo( const umessage::TimerMessage* tm )
 {
 
 }