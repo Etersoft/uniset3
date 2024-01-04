Etersoft/uniset2

(IOController): direct use DBServer (optimization)

extensions/DBServer-PostgreSQL/DBServer_PostgreSQL.cc
@@ -531,7 +531,7 @@ string DBServer_PostgreSQL::getMonitInfo( const string& params )
 }
 //--------------------------------------------------------------------------------------------
 std::shared_ptr<DBServer_PostgreSQL> DBServer_PostgreSQL::init_dbserver( int argc, const char* const* argv,
-        const std::string& prefix )
+    const std::shared_ptr<uniset::SharedMemory>& shm, const std::string& prefix )
 {
     auto conf = uniset_conf();
 
@@ -551,7 +551,10 @@ std::shared_ptr<DBServer_PostgreSQL> DBServer_PostgreSQL::init_dbserver( int arg
     }
 
     uinfo << "(DBServer_PostgreSQL): name = " << name << "(" << ID << ")" << endl;
-    return make_shared<DBServer_PostgreSQL>(ID, prefix);
+    auto db = make_shared<DBServer_PostgreSQL>(ID, prefix);
+    if( shm )
+        shm->setDBServer(db);
+    return db;
 }
 // -----------------------------------------------------------------------------
 void DBServer_PostgreSQL::help_print( int argc, const char* const* argv )

extensions/DBServer-PostgreSQL/DBServer_PostgreSQL.h
@@ -23,6 +23,7 @@
 #include "UniSetTypes.h"
 #include "PostgreSQLInterface.h"
 #include "DBServer.h"
+#include "SharedMemory.h"
 // -------------------------------------------------------------------------
 namespace uniset
 {
@@ -61,7 +62,7 @@ namespace uniset
             virtual ~DBServer_PostgreSQL();
 
             /*! глобальная функция для инициализации объекта */
-            static std::shared_ptr<DBServer_PostgreSQL> init_dbserver( int argc, const char* const* argv, const std::string& prefix = "pgsql" );
+            static std::shared_ptr<DBServer_PostgreSQL> init_dbserver( int argc, const char* const* argv, const std::shared_ptr<uniset::SharedMemory>& ic = nullptr, const std::string& prefix = "pgsql" );
 
             /*! глобальная функция для вывода help-а */
             static void help_print( int argc, const char* const* argv );

include/IOController.h
@@ -31,6 +31,7 @@
 #include "UniSetManager.h"
 #include "Configuration.h"
 #include "Mutex.h"
+#include "DBServer.h"
 //---------------------------------------------------------------------------
 namespace uniset
 {
@@ -41,7 +42,7 @@ namespace uniset
      * Поэтому неизменность ioList во время всей жизни объекта должна гарантироваться.
      * В частности, очень важной является структура USensorInfo, а также userdata,
      * которые используются для "кэширования" (сохранения) указателей на специальные данные.
-     * (см. также IONotifyContoller).
+     * (см. также IONotifyController).
     */
     class IOController:
         public UniSetManager,
@@ -52,6 +53,8 @@ namespace uniset
             IOController( const std::string& name, const std::string& section );
             IOController( const uniset::ObjectId id );
             virtual ~IOController();
+
+            void setDBServer( const std::shared_ptr<uniset::DBServer>& dbserver );
 
             virtual uniset::ObjectType getType() override
             {
@@ -293,6 +296,7 @@ namespace uniset
 
             bool isPingDBServer;    // флаг связи с DBServer-ом
             uniset::ObjectId dbserverID = { uniset::DefaultObjectId };
+            std::shared_ptr<uniset::DBServer> dbserver = { nullptr };
 
             std::mutex loggingMutex; /*!< logging info mutex */
 

Etersoft/uniset3

extensions/DBServer-PostgreSQL/DBServer_PostgreSQL.cc
@@ -513,7 +513,7 @@ string DBServer_PostgreSQL::getMonitInfo( const string& params )
 }
 //--------------------------------------------------------------------------------------------
 std::shared_ptr<DBServer_PostgreSQL> DBServer_PostgreSQL::init_dbserver( int argc, const char* const* argv,
-        const std::string& prefix )
+    const std::shared_ptr<uniset3::SharedMemory>& shm, const std::string& prefix )
 {
     auto conf = uniset_conf();
 
@@ -533,7 +533,10 @@ std::shared_ptr<DBServer_PostgreSQL> DBServer_PostgreSQL::init_dbserver( int arg
     }
 
     uinfo << "(DBServer_PostgreSQL): name = " << name << "(" << ID << ")" << endl;
-    return make_shared<DBServer_PostgreSQL>(ID, prefix);
+    auto db = make_shared<DBServer_PostgreSQL>(ID, prefix);
+    if( shm )
+        shm->setDBServer(db);
+    return db;
 }
 // -----------------------------------------------------------------------------
 void DBServer_PostgreSQL::help_print( int argc, const char* const* argv )

extensions/DBServer-PostgreSQL/DBServer_PostgreSQL.h
@@ -23,6 +23,7 @@
 #include "UniSetTypes.h"
 #include "PostgreSQLInterface.h"
 #include "DBServer.h"
+#include "SharedMemory.h"
 // -------------------------------------------------------------------------
 namespace uniset3
 {
@@ -61,7 +62,7 @@ namespace uniset3
             virtual ~DBServer_PostgreSQL();
 
             /*! глобальная функция для инициализации объекта */
-            static std::shared_ptr<DBServer_PostgreSQL> init_dbserver( int argc, const char* const* argv, const std::string& prefix = "pgsql" );
+            static std::shared_ptr<DBServer_PostgreSQL> init_dbserver( int argc, const char* const* argv, const std::shared_ptr<uniset3::SharedMemory>& ic = nullptr, const std::string& prefix = "pgsql" );
 
             /*! глобальная функция для вывода help-а */
             static void help_print( int argc, const char* const* argv );

include/IOController.h
@@ -31,6 +31,7 @@
 #include "UniSetManager.h"
 #include "Configuration.h"
 #include "Mutex.h"
+#include "DBServer.h"
 //---------------------------------------------------------------------------
 namespace uniset3
 {
@@ -41,7 +42,7 @@ namespace uniset3
      * Поэтому неизменность ioList во время всей жизни объекта должна гарантироваться.
      * В частности, очень важной является структура USensorInfo, а также userdata,
      * которые используются для "кэширования" (сохранения) указателей на специальные данные.
-     * (см. также IONotifyContoller).
+     * (см. также IONotifyController).
     */
     class IOController:
         public UniSetManager,
@@ -54,6 +55,9 @@ namespace uniset3
 
             virtual ::grpc::Status getInfo(::grpc::ServerContext* context, const ::uniset3::GetInfoParams* request, ::google::protobuf::StringValue* response) override;
             virtual std::string getStrType() const override;
+
+            void setDBServer( const std::shared_ptr<uniset3::DBServer>& dbserver );
+
             // ----------------------------------------------------------------
             // Публичный (IDL) интерфейс IOController_i
             // ----------------------------------------------------------------
@@ -241,6 +245,8 @@ namespace uniset3
 
             bool isPingDBServer;    // флаг связи с DBServer-ом
             uniset3::ObjectId dbserverID = { uniset3::DefaultObjectId };
+            std::shared_ptr<uniset3::DBServer> dbserver = { nullptr };
+            ::google::protobuf::Empty empty;
 
             std::mutex loggingMutex; /*!< logging info mutex */
 