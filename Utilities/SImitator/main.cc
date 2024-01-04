Etersoft/uniset2

(simitator): supported "random", supported "--filter-field, --filter-value"

Utilities/SImitator/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 #include <functional>
 #include "Exceptions.h"
 #include "UInterface.h"
@@ -13,13 +14,16 @@ using ModifyFunc = std::function<long(long)>;
 void help_print()
 {
     cout << endl;
-    cout << "--sid id1@Node1,id2,..,idXX@NodeXX  - Аналоговые датчики (AI,AO)" << endl;
+    cout << "--sid id1@Node1,id2,..,idXX@NodeXX  - датчики" << endl;
+    cout << "или" << endl;
+    cout << "--filter-field name     - Считывать список датчиков, только у которых есть поле field" << endl;
+    cout << "--filter-value val      - Считывать список датчиков, только у которых field=value" << endl;
     cout << endl;
-    cout << "--min val       - Нижняя граница датчика. По умолчанию 0" << endl;
-    cout << "--max val       - Верхняя граница датчика. По умолчанию 100 " << endl;
-    cout << "--step val      - Шаг датчика. По умолчанию 1" << endl;
-    cout << "--pause msec    - Пауза. По умолчанию 200 мсек" << endl << endl;
-    cout << "--func [cos|sin] - Функция модификации значения. По умолчания не используется." << endl << endl;
+    cout << "--min val               - Нижняя граница датчика. По умолчанию 0" << endl;
+    cout << "--max val               - Верхняя граница датчика. По умолчанию 100 " << endl;
+    cout << "--step val              - Шаг датчика. По умолчанию 1" << endl;
+    cout << "--pause msec            - Пауза. По умолчанию 200 мсек" << endl << endl;
+    cout << "--func [cos|sin|random] - Функция модификации значения. По умолчания не используется." << endl << endl;
     cout << uniset::Configuration::help() << endl;
 }
 // -----------------------------------------------------------------------------
@@ -59,12 +63,16 @@ int main( int argc, char** argv )
         }
 
         // -------------------------------------
+        std::random_device rnd;
+        std::mt19937 gen(rnd());
 
         auto conf = uniset_init(argc, argv, "configure.xml" );
         UInterface ui(conf);
 
-        const string sid(conf->getArgParam("--sid"));
+        const string sid = conf->getArgParam("--sid");
         auto funcname = conf->getArg2Param("--func", "");
+        const string f_field = conf->getArg2Param("--filter-field", "");
+        const string f_value = conf->getArg2Param("--filter-value", "");
 
         if( !funcname.empty() )
         {
@@ -72,6 +80,10 @@ int main( int argc, char** argv )
                 mf = mf_sin;
             else if( funcname == "cos" )
                 mf = mf_cos;
+            else if( funcname == "random" )
+            {
+
+            }
             else
             {
                 cerr << "Unknown modify function '" << funcname << "'. Must be [sin,cos]" << endl;
@@ -80,40 +92,74 @@ int main( int argc, char** argv )
         }
 
 
-        if( sid.empty() )
+        std::list<ExtInfo> sensors;
+
+        if( !f_field.empty() )
         {
-            cerr << endl << "Use --sid id1,..,idXX" << endl << endl;
-            return 1;
-        }
+            cout << "init sensors with filter: " << f_field << "='" << f_value << "'" << endl;
+            UniXML_iterator it = conf->getXMLSensorsSection();
 
-        auto lst = uniset::getSInfoList(sid, conf);
-
-        if( lst.empty() )
-        {
-            cerr << endl << "Use --sid id1,..,idXX" << endl << endl;
-            return 1;
-        }
-
-        std::list<ExtInfo> l;
-
-        for( auto&& it : lst )
-        {
-            UniversalIO::IOType t = conf->getIOType( it.si.id );
-
-            if( t != UniversalIO::AI && t != UniversalIO::AO )
+            if( !it.goChildren() )
             {
-                cerr << endl << "WARNING! Неверный типа датчика '" << t << "' для id='" << it.fname << "'. Тип должен быть AI или AO." << endl << endl;
-                // return 1;
+                cerr << "not found sensors section in " << conf->getConfFileName() << endl;
+                return 1;
             }
 
-            if( it.si.node == DefaultObjectId )
-                it.si.node = conf->getLocalNode();
+            for( ; it; it++ )
+            {
+                if( uniset::check_filter(it, f_field, f_value) )
+                {
+                    auto id = conf->getSensorID(it.getProp("name"));
 
-            ExtInfo i;
-            i.si = it.si;
-            i.iotype = t;
-            l.push_back(i);
+                    if( id == DefaultObjectId )
+                        continue;
+
+                    ExtInfo i;
+                    i.si.id = id;
+                    i.si.node = conf->getLocalNode();
+                    i.iotype = uniset::getIOType(it.getProp("iotype"));
+                    sensors.push_back(i);
+                }
+            }
+
+            cout << "found " << sensors.size() << " sensors.." << endl;
         }
+        else if( !sid.empty() )
+        {
+            auto lst = uniset::getSInfoList(sid, conf);
+
+            if( lst.empty() )
+            {
+                cerr << endl << "Use --sid id1,..,idXX or --filter-field name [--filter-value value]" << endl << endl;
+                return 1;
+            }
+
+            for( auto&& it : lst )
+            {
+                UniversalIO::IOType t = conf->getIOType( it.si.id );
+
+                if( t != UniversalIO::AI && t != UniversalIO::AO )
+                {
+                    cerr << endl << "WARNING! Неверный типа датчика '" << t << "' для id='" << it.fname << "'. Тип должен быть AI или AO." << endl << endl;
+                    // return 1;
+                }
+
+                if( it.si.node == DefaultObjectId )
+                    it.si.node = conf->getLocalNode();
+
+                ExtInfo i;
+                i.si = it.si;
+                i.iotype = t;
+                sensors.push_back(i);
+            }
+        }
+
+        if( sensors.empty() )
+        {
+            cerr << endl << "Use --sid id1,..,idXX or --filter-field name [--filter-value value]" << endl << endl;
+            return 1;
+        }
+
 
         int amin = conf->getArgInt("--min", "0");
         int amax = conf->getArgInt("--max", "100");
@@ -124,6 +170,16 @@ int main( int argc, char** argv )
             amax = amin;
             amin = temp;
         }
+
+        // init random function
+        std::uniform_int_distribution<> rndgen(amin, amax);
+        ModifyFunc mf_rand = [&rndgen, &gen]( long )
+        {
+            return rndgen(gen);
+        };
+
+        if( funcname == "random" )
+            mf = mf_rand;
 
         int astep = conf->getArgInt("--step", "1");
 
@@ -169,13 +225,13 @@ int main( int argc, char** argv )
 
                 long val = mf(j);
 
-                cout << "\r" << " i = " << j << "     " << flush;
+                cout << "\r" << " i = " << val << "     " << flush;
 
-                for( const auto& it : l )
+                for( const auto& it : sensors )
                 {
                     try
                     {
-                        ui.setValue(it.si, j, DefaultObjectId);
+                        ui.setValue(it.si, val, DefaultObjectId);
                     }
                     catch( const uniset::Exception& ex )
                     {
@@ -198,13 +254,13 @@ int main( int argc, char** argv )
 
                 long val = mf(i);
 
-                cout << "\r" << " i = " << i << "     " << flush;
+                cout << "\r" << " i = " << val << "     " << flush;
 
-                for( std::list<ExtInfo>::iterator it = l.begin(); it != l.end(); ++it )
+                for(std::list<ExtInfo>::iterator it = sensors.begin(); it != sensors.end(); ++it )
                 {
                     try
                     {
-                        ui.setValue(it->si, i, DefaultObjectId);
+                        ui.setValue(it->si, val, DefaultObjectId);
                     }
                     catch( const uniset::Exception& ex )
                     {

Utilities/SImitator/stop.sh
@@ -1,0 +1,1 @@
+../scripts/uniset2-stop.sh

Etersoft/uniset3

Utilities/SImitator/main.cc
@@ -1,26 +1,29 @@
 #include <iostream>
-#include <algorithm>
 #include <cmath>
+#include <random>
 #include <functional>
 #include "Exceptions.h"
 #include "UInterface.h"
-#include "IOController.grpc.pb.h"
 // -----------------------------------------------------------------------------
 using namespace std;
 using namespace uniset3;
 
 using ModifyFunc = std::function<long(long)>;
+
 // -----------------------------------------------------------------------------
 void help_print()
 {
     cout << endl;
-    cout << "--sid id1@Node1,id2,..,idXX@NodeXX  - Аналоговые датчики (AI,AO)" << endl;
+    cout << "--sid id1@Node1,id2,..,idXX@NodeXX  - датчики" << endl;
+    cout << "или" << endl;
+    cout << "--filter-field name     - Считывать список датчиков, только у которых есть поле field" << endl;
+    cout << "--filter-value val      - Считывать список датчиков, только у которых field=value" << endl;
     cout << endl;
-    cout << "--min val       - Нижняя граница датчика. По умолчанию 0" << endl;
-    cout << "--max val       - Верхняя граница датчика. По умолчанию 100 " << endl;
-    cout << "--step val      - Шаг датчика. По умолчанию 1" << endl;
-    cout << "--pause msec    - Пауза. По умолчанию 200 мсек" << endl << endl;
-    cout << "--func [cos|sin] - Функция модификации значения. По умолчания не используется." << endl << endl;
+    cout << "--min val               - Нижняя граница датчика. По умолчанию 0" << endl;
+    cout << "--max val               - Верхняя граница датчика. По умолчанию 100 " << endl;
+    cout << "--step val              - Шаг датчика. По умолчанию 1" << endl;
+    cout << "--pause msec            - Пауза. По умолчанию 200 мсек" << endl << endl;
+    cout << "--func [cos|sin|random] - Функция модификации значения. По умолчания не используется." << endl << endl;
     cout << uniset3::Configuration::help() << endl;
 }
 // -----------------------------------------------------------------------------
@@ -60,12 +63,16 @@ int main( int argc, char** argv )
         }
 
         // -------------------------------------
+        std::random_device rnd;
+        std::mt19937 gen(rnd());
 
         auto conf = uniset_init(argc, argv, "configure.xml" );
         UInterface ui(conf);
 
-        const string sid(conf->getArgParam("--sid"));
+        const string sid = conf->getArgParam("--sid");
         auto funcname = conf->getArg2Param("--func", "");
+        const string f_field = conf->getArg2Param("--filter-field", "");
+        const string f_value = conf->getArg2Param("--filter-value", "");
 
         if( !funcname.empty() )
         {
@@ -73,6 +80,10 @@ int main( int argc, char** argv )
                 mf = mf_sin;
             else if( funcname == "cos" )
                 mf = mf_cos;
+            else if( funcname == "random" )
+            {
+
+            }
             else
             {
                 cerr << "Unknown modify function '" << funcname << "'. Must be [sin,cos]" << endl;
@@ -81,43 +92,74 @@ int main( int argc, char** argv )
         }
 
 
-        if( sid.empty() )
+        std::list<ExtInfo> sensors;
+
+        if( !f_field.empty() )
         {
-            cerr << endl << "Use --sid id1,..,idXX" << endl << endl;
-            return 1;
-        }
+            cout << "init sensors with filter: " << f_field << "='" << f_value << "'" << endl;
+            UniXML_iterator it = conf->getXMLSensorsSection();
 
-        auto lst = uniset3::getSInfoList(sid, conf);
-
-        if( lst.empty() )
-        {
-            cerr << endl << "Use --sid id1,..,idXX" << endl << endl;
-            return 1;
-        }
-
-        std::list<ExtInfo> l;
-        bool useSync = false;
-
-        for( auto&& it : lst )
-        {
-            uniset3::IOType t = conf->getIOType( it.si.id() );
-
-            if( t != uniset3::AI && t != uniset3::AO )
+            if( !it.goChildren() )
             {
-                cerr << endl << "WARNING! Неверный типа датчика '" << t << "' для id='" << it.fname << "'. Тип должен быть AI или AO." << endl << endl;
-                // return 1;
+                cerr << "not found sensors section in " << conf->getConfFileName() << endl;
+                return 1;
             }
 
-            if( it.si.node() == DefaultObjectId )
-                it.si.set_node(conf->getLocalNode());
-            else
-                useSync = true; // если хоть один датчик на другом узле, используем удалённый вызов
+            for( ; it; it++ )
+            {
+                if( uniset3::check_filter(it, f_field, f_value) )
+                {
+                    auto id = conf->getSensorID(it.getProp("name"));
 
-            ExtInfo i;
-            i.si = it.si;
-            i.iotype = t;
-            l.push_back(i);
+                    if( id == DefaultObjectId )
+                        continue;
+
+                    ExtInfo i;
+                    i.si.set_id(id);
+                    i.si.set_node(conf->getLocalNode());
+                    i.iotype = uniset3::getIOType(it.getProp("iotype"));
+                    sensors.push_back(i);
+                }
+            }
+
+            cout << "found " << sensors.size() << " sensors.." << endl;
         }
+        else if( !sid.empty() )
+        {
+            auto lst = uniset3::getSInfoList(sid, conf);
+
+            if( lst.empty() )
+            {
+                cerr << endl << "Use --sid id1,..,idXX or --filter-field name [--filter-value value]" << endl << endl;
+                return 1;
+            }
+
+            for( auto&& it : lst )
+            {
+                uniset3::IOType t = conf->getIOType( it.si.id() );
+
+                if( t != uniset3::AI && t != uniset3::AO )
+                {
+                    cerr << endl << "WARNING! Неверный типа датчика '" << t << "' для id='" << it.fname << "'. Тип должен быть AI или AO." << endl << endl;
+                    // return 1;
+                }
+
+                if( it.si.node() == DefaultObjectId )
+                    it.si.set_node(conf->getLocalNode());
+
+                ExtInfo i;
+                i.si = it.si;
+                i.iotype = t;
+                sensors.push_back(i);
+            }
+        }
+
+        if( sensors.empty() )
+        {
+            cerr << endl << "Use --sid id1,..,idXX or --filter-field name [--filter-value value]" << endl << endl;
+            return 1;
+        }
+
 
         int amin = conf->getArgInt("--min", "0");
         int amax = conf->getArgInt("--max", "100");
@@ -129,6 +171,16 @@ int main( int argc, char** argv )
             amin = temp;
         }
 
+        // init random function
+        std::uniform_int_distribution<> rndgen(amin, amax);
+        ModifyFunc mf_rand = [&rndgen, &gen]( long )
+        {
+            return rndgen(gen);
+        };
+
+        if( funcname == "random" )
+            mf = mf_rand;
+
         int astep = conf->getArgInt("--step", "1");
 
         if( astep <= 0 )
@@ -139,7 +191,7 @@ int main( int argc, char** argv )
 
         int amsec = conf->getArgInt("--pause", "200");
 
-        if( amsec <= 10 )
+        if(amsec <= 10)
         {
             cerr << endl << "Ошибка, используйте --pause val - любое положительное число > 10" << endl << endl;
             return 1;
@@ -153,69 +205,14 @@ int main( int argc, char** argv )
         cout << "  max = " << amax << endl;
         cout << "  step = " << astep << endl;
         cout << "  pause = " << amsec << endl;
-        cout << "  sync_call = " << useSync << endl;
 
         if( !funcname.empty() )
             cout << "  function = " << funcname << endl;
 
         cout << "------------------------------" << endl << endl;
 
-        auto oref = ui.resolve(lst.begin()->si.id());
-
-        if( !oref->c )
-        {
-            cerr << "can't resolve server: " << oref->ref.addr() << endl;
-            return 1;
-        }
-
-        std::unique_ptr<IONotifyStreamController_i::Stub> stub(IONotifyStreamController_i::NewStub(oref->c));
-
-        grpc::ClientContext ctx;
-        SensorsStreamCmd request;
-        request.set_cmd(uniset3::UIOSet);
-
-        for( const auto& i : lst )
-        {
-            auto s = request.add_slist();
-            s->set_id(i.si.id());
-            s->set_val(i.val);
-        }
-
-        typedef std::unique_ptr< ::grpc::ClientReaderWriter< ::uniset3::SensorsStreamCmd, ::uniset3::umessage::SensorMessage>> StreamType;
-
-        StreamType stream;
-
-        if( !useSync )
-            stream = stub->sensorsStream(&ctx);
-
-        auto fSync = std::function<void(int)>([&](int i)
-        {
-            for( const auto& it : l )
-            {
-                try
-                {
-                    ui.setValue(it.si, i, DefaultObjectId);
-                }
-                catch( const uniset3::Exception& ex )
-                {
-                    cerr << endl << "save id=" << it.fname << " " << ex << endl;
-                }
-            }
-        });
-
-        auto fAsync = std::function<void(int)>([&](int i)
-        {
-            for( const auto& it : l )
-            {
-                for( int k = 0; k < request.slist_size(); k++ )
-                    request.mutable_slist(k)->set_val(i);
-
-                if( !stream->Write(request) )
-                    cerr << endl << "write error.." << endl;
-            }
-        });
-
         int i = amin - astep, j = amax;
+
 
         while(1)
         {
@@ -228,12 +225,19 @@ int main( int argc, char** argv )
 
                 long val = mf(j);
 
-                cout << "\r" << " i = " << j << "     " << flush;
+                cout << "\r" << " i = " << val << "     " << flush;
 
-                if( useSync )
-                    fSync(j);
-                else
-                    fAsync(j);
+                for( const auto& it : sensors )
+                {
+                    try
+                    {
+                        ui.setValue(it.si, val, DefaultObjectId);
+                    }
+                    catch( const uniset3::Exception& ex )
+                    {
+                        cerr << endl << "save id=" << it.fname << " " << ex << endl;
+                    }
+                }
 
                 if(j <= amin)
                 {
@@ -250,12 +254,19 @@ int main( int argc, char** argv )
 
                 long val = mf(i);
 
-                cout << "\r" << " i = " << i << "     " << flush;
+                cout << "\r" << " i = " << val << "     " << flush;
 
-                if( useSync )
-                    fSync(i);
-                else
-                    fAsync(i);
+                for(std::list<ExtInfo>::iterator it = sensors.begin(); it != sensors.end(); ++it )
+                {
+                    try
+                    {
+                        ui.setValue(it->si, val, DefaultObjectId);
+                    }
+                    catch( const uniset3::Exception& ex )
+                    {
+                        cerr << endl << "save id=" << it->fname << " " << ex << endl;
+                    }
+                }
             }
 
             msleep(amsec);

Utilities/SImitator/stop.sh
@@ -1,0 +1,1 @@
+../scripts/uniset3-stop.sh