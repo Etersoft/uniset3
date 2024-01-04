Etersoft/uniset2

(uwebsocket): supported "freeze/unfreeze"

extensions/UWebSocketGate/UWebSocketGate.cc
@@ -1110,6 +1110,19 @@ void UWebSocketGate::UWebSocket::set( uniset::ObjectId id, long value )
     qcmd.push(s);
 }
 // -----------------------------------------------------------------------------
+void UWebSocketGate::UWebSocket::freeze( uniset::ObjectId id, long value )
+{
+    auto s = std::make_shared<sinfo>("freeze", id);
+    s->value = value;
+    qcmd.push(s);
+}
+// -----------------------------------------------------------------------------
+void UWebSocketGate::UWebSocket::unfreeze( uniset::ObjectId id )
+{
+    auto s = std::make_shared<sinfo>("unfreeze", id);
+    qcmd.push(s);
+}
+// -----------------------------------------------------------------------------
 void UWebSocketGate::UWebSocket::get( uniset::ObjectId id )
 {
     qcmd.push(std::make_shared<sinfo>("get", id));
@@ -1178,6 +1191,14 @@ void UWebSocketGate::UWebSocket::doCommand( const std::shared_ptr<SMInterface>& 
             else if( s->cmd == "set" )
             {
                 ui->setValue(s->id, s->value);
+            }
+            else if( s->cmd == "freeze" )
+            {
+                ui->freezeValue(s->id, true, s->value, ui->ID());
+            }
+            else if( s->cmd == "unfreeze" )
+            {
+                ui->freezeValue(s->id, false, 0, ui->ID());
             }
             else if( s->cmd == "get" )
             {
@@ -1268,8 +1289,18 @@ void UWebSocketGate::UWebSocket::onCommand( std::string_view cmdtxt )
         return;
     }
 
-    string_view cmd = cmdtxt.substr(0, 3);
-    string_view params = cmdtxt.substr(4);
+    auto cpos = cmdtxt.find_first_of(':');
+    if( cpos == string_view::npos )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << " error: bad command format '" << cmdtxt << "'. Must be: 'cmd:params'" << endl;
+
+        sendError("Bad command format. Command must be 'cmd:params'");
+        return;
+    }
+
+    string_view cmd = cmdtxt.substr(0, cpos);
+    string_view params = cmdtxt.substr(cpos+1);
 
     myinfoV(3) << "(websocket)(command): " << req->clientAddress().toString()
                << "(" << cmd << "): " << params << endl;
@@ -1322,6 +1353,32 @@ void UWebSocketGate::UWebSocket::onCommand( std::string_view cmdtxt )
 
         for( const auto& id : idlist.ref() )
             get(id);
+
+        // уведомление о новой команде
+        cmdsignal->send();
+    }
+    else if( cmd == "freeze" )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << "(freeze): " << params << endl;
+
+        auto idlist = uniset::getSInfoList_sv(params, uniset_conf());
+
+        for( const auto& i : idlist )
+            freeze(i.si.id, i.val);
+
+        // уведомление о новой команде
+        cmdsignal->send();
+    }
+    else if( cmd == "unfreeze" )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << "(unfreeze): " << params << endl;
+
+        auto idlist = uniset::split_by_id(params);
+
+        for( const auto& id: idlist.ref() )
+            unfreeze(id);
 
         // уведомление о новой команде
         cmdsignal->send();

extensions/UWebSocketGate/UWebSocketGate.h
@@ -171,6 +171,8 @@ namespace uniset
         - "ask:id1,id2,name3,..." - подписаться на уведомления об изменении датчиков (sensorInfo)
         - "del:id1,id2,name3,..." - отказаться от уведомления об изменении датчиков
         - "get:id1,id2,name3,..." - получить текущее значение датчиков (разовое сообщение ShortSensorInfo)
+        - "freeze:id1=val1,id2=val2,name3=val4,..." - выставить значение и заморозить изменение датчиков
+        - "unfreeze:id1,id2,name3..." - разморозить изменения датчиков
 
         Если длина команды превышает допустимое значение, то возвращается ошибка
         \code
@@ -328,6 +330,8 @@ namespace uniset
                     void del( uniset::ObjectId id );
                     void get( uniset::ObjectId id );
                     void set( uniset::ObjectId id, long value );
+                    void freeze( uniset::ObjectId id, long value );
+                    void unfreeze( uniset::ObjectId id );
                     void sensorInfo( const uniset::SensorMessage* sm );
                     void doCommand( const std::shared_ptr<SMInterface>& ui );
                     static Poco::JSON::Object::Ptr to_short_json( const std::shared_ptr<sinfo>& si );

extensions/include/SMInterface.h
@@ -44,6 +44,8 @@ namespace uniset
             void askSensor( uniset::ObjectId id, UniversalIO::UIOCommand cmd,
                             uniset::ObjectId backid = uniset::DefaultObjectId );
 
+            void freezeValue( uniset::ObjectId id, bool set, long value, uniset::ObjectId supplier );
+
             IOController_i::SensorInfoSeq* getSensorsMap();
             IONotifyController_i::ThresholdsListSeq* getThresholdsList();
 

Etersoft/uniset3

extensions/UWebSocketGate/UWebSocketGate.cc
@@ -1111,6 +1111,19 @@ void UWebSocketGate::UWebSocket::set( uniset3::ObjectId id, long value )
     qcmd.push(s);
 }
 // -----------------------------------------------------------------------------
+void UWebSocketGate::UWebSocket::freeze( uniset3::ObjectId id, long value )
+{
+    auto s = std::make_shared<sinfo>("freeze", id);
+    s->value = value;
+    qcmd.push(s);
+}
+// -----------------------------------------------------------------------------
+void UWebSocketGate::UWebSocket::unfreeze( uniset3::ObjectId id )
+{
+    auto s = std::make_shared<sinfo>("unfreeze", id);
+    qcmd.push(s);
+}
+// -----------------------------------------------------------------------------
 void UWebSocketGate::UWebSocket::get( uniset3::ObjectId id )
 {
     qcmd.push(std::make_shared<sinfo>("get", id));
@@ -1179,6 +1192,14 @@ void UWebSocketGate::UWebSocket::doCommand( const std::shared_ptr<SMInterface>& 
             else if( s->cmd == "set" )
             {
                 ui->setValue(s->id, s->value);
+            }
+            else if( s->cmd == "freeze" )
+            {
+                ui->freezeValue(s->id, true, s->value, ui->ID());
+            }
+            else if( s->cmd == "unfreeze" )
+            {
+                ui->freezeValue(s->id, false, 0, ui->ID());
             }
             else if( s->cmd == "get" )
             {
@@ -1269,8 +1290,19 @@ void UWebSocketGate::UWebSocket::onCommand( std::string_view cmdtxt )
         return;
     }
 
-    string_view cmd = cmdtxt.substr(0, 3);
-    string_view params = cmdtxt.substr(4);
+    auto cpos = cmdtxt.find_first_of(':');
+
+    if( cpos == string_view::npos )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << " error: bad command format '" << cmdtxt << "'. Must be: 'cmd:params'" << endl;
+
+        sendError("Bad command format. Command must be 'cmd:params'");
+        return;
+    }
+
+    string_view cmd = cmdtxt.substr(0, cpos);
+    string_view params = cmdtxt.substr(cpos + 1);
 
     myinfoV(3) << "(websocket)(command): " << req->clientAddress().toString()
                << "(" << cmd << "): " << params << endl;
@@ -1324,6 +1356,32 @@ void UWebSocketGate::UWebSocket::onCommand( std::string_view cmdtxt )
 
         for( const auto& id : idlist.ref() )
             get(id);
+
+        // уведомление о новой команде
+        cmdsignal->send();
+    }
+    else if( cmd == "freeze" )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << "(freeze): " << params << endl;
+
+        auto idlist = uniset3::getSInfoList_sv(params, uniset_conf());
+
+        for( const auto& i : idlist )
+            freeze(i.si.id(), i.val);
+
+        // уведомление о новой команде
+        cmdsignal->send();
+    }
+    else if( cmd == "unfreeze" )
+    {
+        myinfoV(3) << "(websocket): " << req->clientAddress().toString()
+                   << "(unfreeze): " << params << endl;
+
+        auto idlist = uniset3::split_by_id(params);
+
+        for( const auto& id : idlist.ref() )
+            unfreeze(id);
 
         // уведомление о новой команде
         cmdsignal->send();

extensions/UWebSocketGate/UWebSocketGate.h
@@ -171,6 +171,8 @@ namespace uniset3
         - "ask:id1,id2,name3,..." - подписаться на уведомления об изменении датчиков (sensorInfo)
         - "del:id1,id2,name3,..." - отказаться от уведомления об изменении датчиков
         - "get:id1,id2,name3,..." - получить текущее значение датчиков (разовое сообщение ShortSensorInfo)
+        - "freeze:id1=val1,id2=val2,name3=val4,..." - выставить значение и заморозить изменение датчиков
+        - "unfreeze:id1,id2,name3..." - разморозить изменения датчиков
 
         Если длина команды превышает допустимое значение, то возвращается ошибка
         \code
@@ -329,6 +331,8 @@ namespace uniset3
                     void del( uniset3::ObjectId id );
                     void get( uniset3::ObjectId id );
                     void set( uniset3::ObjectId id, long value );
+                    void freeze( uniset3::ObjectId id, long value );
+                    void unfreeze( uniset3::ObjectId id );
                     void sensorInfo( const uniset3::umessage::SensorMessage* sm );
                     void doCommand( const std::shared_ptr<SMInterface>& ui );
 

extensions/include/SMInterface.h
@@ -46,6 +46,8 @@ namespace uniset3
 
             uniset3::SensorIOInfoSeq getSensorsMap();
 
+            void freezeValue( uniset3::ObjectId id, bool set, long value, uniset3::ObjectId supplier );
+
             void localSetValue( IOController::IOStateList::iterator& it,
                                 uniset3::ObjectId sid,
                                 long newvalue, uniset3::ObjectId sup_id );