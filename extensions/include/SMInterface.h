Etersoft/uniset2

(utils): "cancelled" -> "canceled"

extensions/include/SMInterface.h
@@ -69,6 +69,7 @@ namespace uniset
             bool waitSMready( int msec, int pause = 5000 );
             bool waitSMworking( uniset::ObjectId, int msec, int pause = 3000 );
             bool waitSMreadyWithCancellation( int msec, std::atomic_bool& cancelFlag, int pause = 5000 );
+            bool waitSMworkingWithCancellation( uniset::ObjectId sid, int ready_timeout, std::atomic_bool& cancelFlag, int pmsec );
 
             inline bool isLocalwork() const noexcept
             {

extensions/lib/SMInterface.cc
@@ -297,6 +297,27 @@ bool SMInterface::waitSMworking( uniset::ObjectId sid, int msec, int pmsec )
     return sm_ready;
 }
 // --------------------------------------------------------------------------
+bool SMInterface::waitSMworkingWithCancellation( uniset::ObjectId sid, int ready_timeout, std::atomic_bool& cancelFlag, int pmsec )
+{
+    PassiveTimer ptSMready(ready_timeout);
+    bool sm_ready = false;
+
+    while( !ptSMready.checkTime() && !sm_ready && !cancelFlag )
+    {
+        try
+        {
+            getValue(sid);
+            sm_ready = true;
+            break;
+        }
+        catch(...) {}
+
+        msleep(pmsec);
+    }
+
+    return sm_ready;
+}
+// --------------------------------------------------------------------------
 bool SMInterface::waitSMreadyWithCancellation(int ready_timeout, std::atomic_bool& cancelFlag, int pmsec)
 {
     PassiveTimer ptSMready(ready_timeout);

Etersoft/uniset3

extensions/include/SMInterface.h
@@ -61,6 +61,7 @@ namespace uniset3
             bool waitSMready( int msec, int pause = 5000 );
             bool waitSMworking( uniset3::ObjectId, int msec, int pause = 3000 );
             bool waitSMreadyWithCancellation( int msec, std::atomic_bool& cancelFlag, int pause = 5000 );
+            bool waitSMworkingWithCancellation( uniset3::ObjectId sid, int ready_timeout, std::atomic_bool& cancelFlag, int pmsec );
 
             inline bool isLocalwork() const noexcept
             {

extensions/lib/SMInterface.cc
@@ -259,6 +259,27 @@ bool SMInterface::waitSMworking( uniset3::ObjectId sid, int msec, int pmsec )
     return sm_ready;
 }
 // --------------------------------------------------------------------------
+bool SMInterface::waitSMworkingWithCancellation( uniset3::ObjectId sid, int ready_timeout, std::atomic_bool& cancelFlag, int pmsec )
+{
+    PassiveTimer ptSMready(ready_timeout);
+    bool sm_ready = false;
+
+    while( !ptSMready.checkTime() && !sm_ready && !cancelFlag )
+    {
+        try
+        {
+            getValue(sid);
+            sm_ready = true;
+            break;
+        }
+        catch(...) {}
+
+        msleep(pmsec);
+    }
+
+    return sm_ready;
+}
+// --------------------------------------------------------------------------
 bool SMInterface::waitSMreadyWithCancellation(int ready_timeout, std::atomic_bool& cancelFlag, int pmsec)
 {
     PassiveTimer ptSMready(ready_timeout);